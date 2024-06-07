d2fb49a27d41d4fac02a066539b149be4b61900a
guidoftp <147518664+guidoftp@users.noreply.github.com>
2024-06-07 08:39:30 -0400

    Primera parte

---
+++ asset/js/credenciale.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCDCzi6h_OUFfwKrXDyCl38ZRTP0J067Dg",
+    authDomain: "eva3-frond-end.firebaseapp.com",
+    projectId: "eva3-frond-end",
+    storageBucket: "eva3-frond-end.appspot.com",
+    messagingSenderId: "658725734149",
+    appId: "1:658725734149:web:b8c52d47433118fb13596c"
+  };
---
+++ asset/js/firebase.js
@@ -0,0 +1,18 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCDCzi6h_OUFfwKrXDyCl38ZRTP0J067Dg",
+    authDomain: "eva3-frond-end.firebaseapp.com",
+    projectId: "eva3-frond-end",
+    storageBucket: "eva3-frond-end.appspot.com",
+    messagingSenderId: "658725734149",
+    appId: "1:658725734149:web:b8c52d47433118fb13596c"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
