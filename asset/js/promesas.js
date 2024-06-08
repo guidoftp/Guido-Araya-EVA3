1cf9b4f4253810c86397f83fa07b0fbf736cdd0b
Guido Andres Araya Gutierrez <guidoandres62@gmail.com>
2024-06-07 22:55:40 -0400

    REGISTRO TERMINADO
    falta arreglar un par de errores como el por que registra pero me salta el "Ups, tuviste un problema" y hacer funcion cargarDatos().

--- asset/js/promesas.js
+++ asset/js/promesas.js
@@ -1,3 +1,21 @@
-import { doc, getDoc} from "firebase/firestore"
+import { collection, addDoc, getDocs, doc, updateDoc, deleteDoc} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js"; //corregi lo de firestore y el link
 import { db } from "./firebase.js";
+
+export const registrarGuerrero = async (guerrero) => {
+    console.log("guerrero");
+    console.log(guerrero);
+    const docRef = await addDoc(collection(db, "guerrero"), guerrero);
+    return docRef
+}
+export const obtenerPersona = async() =>{
+    const ref=collection(db,"guerrero");
+    const querySnap = await getDocs(ref);
+    console.log(querySnap);
+    let listado = []
+    querySnap.forEach(doc => {
+        listado.push({...doc.data(),id:doc.id});
+    });
+    return listado;
+    
+}
 
--- asset/js/script.js
+++ asset/js/script.js
@@ -1,14 +1,68 @@
+import {registrarGuerrero } from "./promesas.js"
 window.addEventListener("load", ()=>{
     document.getElementById("btnRegistrar").addEventListener("click",registrar);
-})
+});
 
 const registrar = () =>{
-    let eNombre = document.getElementById("nombre")
+    let eNombre = document.getElementById("nombre");
+    let eEdad = document.getElementById("edad")
+    let eEmail = document.getElementById("email")
+    let eRut = document.getElementById("rut")
+    let ePersonaje = document.getElementById("personaje")
+    let eHabilidad = document.getElementById("habilidad")
+
+    let eOpening;
+    if (document.getElementById("opening1").checked){
+        eOpening="CHA-LA HEAD-CHA-LA";
+    } else if(document.getElementById("opening2").checked){
+        eOpening="WE GOTTA POWER";
+    } else if(document.getElementById("opening3").checked){
+        eOpening="EL PODER NUESTRO ES";
+    }
+
+    let eRaza;
+    if (document.getElementById("raza1").checked){
+        eRaza="Saijayins";
+    } else if (document.getElementById("raza2").checked){
+        eRaza="Androides";
+    } else if (document.getElementById("raza3").checked){
+        eRaza="Humanos";
+    }
+
+    let eSaga = document.getElementById("saga");
 
     let vNombre = eNombre.value;
+    let vEdad = eEdad.value;
+    let vEmail = eEmail.value;
+    let vRut = eRut.value;
+    let vPersonaje = ePersonaje.value;
+    let vHabilidad = eHabilidad.value;
+    let vOpening = eOpening;
+    let vRaza = eRaza;
+    let vSaga = eSaga.value;
 
     //recuperar el objeto
-    let objeto{
-        nombre: vNombre;
-    }
+    let objeto = {
+        nombre: vNombre,
+        edad: vEdad,
+        email: vEmail, 
+        rut: vRut,
+        personaje: vPersonaje,
+        habilidad: vHabilidad,
+        opening: vOpening,
+        raza: vRaza,
+        saga: vSaga,
+
+    };
+
+    registrarGuerrero(objeto).then(()=>{
+        alert ("Se registro el Guerrero Z");
+        cargarDatos();
+    }).catch((r)=>{
+        alert ("Ups, tuviste un problema");
+        alert (r);
+        console.log(r)
+    });
 }
+
+//cargar datos
